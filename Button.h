562b4bf80df939b31b9c324095698d3e8cde8870
The Wind <mr.tarnado4@gmail.com>
2022-02-08 21:42:26 +0200

    Widget interface (commented)

--- Button.h
+++ Button.h
@@ -2,20 +2,57 @@
 #include <SFML/Graphics.hpp>
 #include "Widgets.h"
 
+/// <summary>
+/// Standard button class
+/// </summary>
 class Button : public Widget
 {
-protected:
-	std::string text;
-
 public:
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	/// <param name="win">-- Window in whith to display</param>
+	/// <param name="texture">-- texture of button</param>
+	/// <param name="size">-- size of button (in pixels)</param>
+	/// <param name="position">-- position of button in window (in pixels)</param>
+	/// <param name="col">-- widget color</param>
+	/// <param name="text">-- text in button</param>
+	Button(sf::RenderWindow* win, sf::Texture& texture, Size size, Position position, std::string text, sf::Color color);
 
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	/// <param name="win">-- Window in whith to display</param>
+	/// <param name="texture">-- texture of button</param>
+	/// <param name="size">-- size of button (in pixels)</param>
+	/// <param name="position">-- position of button in window (in pixels)</param>
+	/// <param name="text">-- text in button</param>
 	Button(sf::RenderWindow *win, sf::Texture &texture, Size size, Position position, std::string text);
 
-	Button(sf::RenderWindow *win, sf::Texture &texture, Size size, Position position, std::string text, sf::Color color);
-
+	/// <summary>
+	/// Constructor without input settings
+	/// 
+	/// Standard settings:
+	/// Position: X=0; Y=0.
+	/// Size: width=120; height=80.
+	/// without Color
+	/// 
+	/// </summary>
+	/// <param name="win">-- Window in whith to display</param>
+	/// <param name="texture">-- texture of button</param>
 	Button(sf::RenderWindow *win, sf::Texture &texture);
 
 
+	/// <summary>
+	/// Draw button
+	/// </summary>
 	void draw();
+
+protected:
+	/// <summary>
+	/// Text in button
+	/// (don't work yet)
+	/// </summary>
+	std::string text;
 
 };
--- Widgets.h
+++ Widgets.h
@@ -3,6 +3,9 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+/// <summary>
+/// Struct for storage position data (in pixels)
+/// </summary>
 struct Position
 {
 	float X, Y;
@@ -20,6 +23,9 @@ struct Position
 	}
 };
 
+/// <summary>
+/// Struct for storage sie data (in pixels)
+/// </summary>
 struct Size
 {
 	int width, height;
@@ -37,48 +43,122 @@ struct Size
 	}
 };
 
+
+/// <summary>
+/// Base class for widgets
+/// </summary>
 class Widget
 {
-protected:
-	sf::Sprite sprite;
-	sf::Texture texture;
-	sf::RenderWindow *window;
-
-	Size size;
-	Position position;
-	sf::Color color;
-
 public:
-
-	Widget(sf::RenderWindow *win);
-
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	/// <param name="win">-- Window in whith to display</param>
+	/// <param name="siz">-- size of widget (in pixels)</param>
+	/// <param name="pos">-- position of widget in window (in pixels)</param>
+	/// <param name="col">-- widget color</param>
 	Widget(sf::RenderWindow *win, Size siz, Position pos, sf::Color col);
 
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	/// <param name="win">-- Window in whith to display</param>
+	/// <param name="siz">-- size of widget (in pixels)</param>
+	/// <param name="pos">-- position of widget in window (in pixels)</param>
+	/// <param name="col">-- widget color</param>
 	Widget(sf::RenderWindow *win, Size siz, Position pos);
 
+	/// <summary>
+	/// Constructor without input settings
+	/// 
+	/// Standard settings:
+	/// Position: X=0; Y=0.
+	/// Size: width=120; height=80.
+	/// without Color
+	/// without Texture
+	/// 
+	/// </summary>
+	/// <param name="win">-- Window in whith to display</param>
+	Widget(sf::RenderWindow* win);
+
+	/// <summary>
+	/// Widget size getter
+	/// </summary>
 	Size getSize();
 
+	/// <summary>
+	/// Widget size setter
+	/// </summary>
 	void setSize(Size siz);
 
+	/// <summary>
+	/// Widget size setter
+	/// </summary>
 	void setSize(int x, int y);
 
+	/// <summary>
+	/// Widget position getter
+	/// </summary>
 	Position getPosition();
 
+	/// <summary>
+	/// Widget position setter
+	/// </summary>
 	void setPosition(Position pos);
 
+	/// <summary>
+	/// Widget position setter
+	/// </summary>
 	void setPosition(float X, float Y);
 
+	/// <summary>
+	/// Widget color getter
+	/// </summary>
 	sf::Color getColor();
 
+	/// <summary>
+	/// Widget color setter
+	/// </summary>
 	void setColor(int r, int g, int b, int w);
 
+	/// <summary>
+	/// Widget color setter
+	/// </summary>
 	void setColor(sf::Color col);
 
+	/// <summary>
+	/// Widget texture getter
+	/// </summary>
 	sf::Texture* getTexture();
 
+	/// <summary>
+	/// Widget texture setter
+	/// </summary>
 	void setTexture();
 
+	/// <summary>
+	/// Widget texture setter
+	/// </summary>
 	void setTexture(sf::Texture &tex);
 
+	/// <summary>
+	/// Draw widget
+	/// </summary>
 	void draw();
+
+protected:
+	/// <summary> sprite of widget </summary>
+	sf::Sprite sprite;
+	/// <summary> texture of widget </summary>
+	sf::Texture texture;
+	/// <summary> link to the window in wich the widget is located </summary>
+	sf::RenderWindow* window;
+
+	/// <summary> size of widget </summary>
+	Size size;
+	/// <summary> position of widget in window </summary>
+	Position position;
+	/// <summary> color of widget </summary>
+	sf::Color color;
+
 };
